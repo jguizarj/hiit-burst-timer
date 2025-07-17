50bfadfcaf57cd8c445ffe794248a1a5d2c521ec
Jonathan Guizar <53882278+jguizarj@users.noreply.github.com>
2025-07-17 12:56:29 -0600

    chore: Add Prettier and Prettier plugin for Tailwind CSS to devDependencies

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindStylesheet: "./src/main.css",
+};
+
+export default config;
